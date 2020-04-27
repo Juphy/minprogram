b34358794ff4799e36732abbd94a1f3714ea79da
wenquanpage <825414435@qq.com>
2020-04-27 18:10:28 +0800

    个人中心新增界面设计

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -4,7 +4,21 @@ var app = getApp()
 Page({
   data: {
     motto: '活动让生活更美好!',
-    userInfo: {}
+    userInfo: {},
+    mineMenuList:[
+      {
+        title: '发起人信息管理',
+        desc: '奖品类型通常为实物、优惠券、现金',
+        icon: '../../image/customermanager.png',
+        url: ''
+      },
+      {
+        title: '审核抽奖活动',
+        desc: '仅限公众号粉丝抽奖功能，吸粉',
+        icon: '../../image/examine.png',
+        url: ''
+      }
+    ]
   },
   //事件处理函数
   bindViewTap: function () {
@@ -15,4 +29,5 @@ Page({
   onLoad: function () {
     app.getUserInfo(userInfo => this.setData({ userInfo }))
   }
+
 })
