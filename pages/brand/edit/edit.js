2b39973815189a6695f48cb7d107de253e4ddf4e
liwei <liwei@huayingjuhe.com>
2020-05-20 12:13:40 +0800

    品牌信息

--- pages/brand/edit/edit.js
+++ pages/brand/edit/edit.js
@@ -7,21 +7,17 @@ const Promote = Data.Promotes;
 Page({
   data: {
     state: 0,
-    promote1: {
-      promote_type: 1,
+    promote: {
+      id: 0,
       promote_name: '',
+      promote_type: null,
       promote_guide: '',
-      id: null
+      promote_img: null
     },
-    promote2: {
-      promote_type: 2,
-      promote_name: '',
-      promote_guide: '',
-      promote_img: '',
-      id: null
-    },
-    Promotes: [],
-    Promote: {}
+    name: '',
+    logo: '',
+    describe: '',
+    introduce: ''
   },
 
   onLoad: function (options) {
@@ -47,91 +43,51 @@ Page({
     }
   },
 
-  getPromoteInfo(data, callback) {
-    Api.fetchPost(Api.PromoteInfo, data, (err, res) => {
-      if (res.status === 200) {
-        callback(res.result);
-      }
+  // onShow: function () {
+  //   let pages = getCurrentPages();
+  //   var prepage = pages[pages.length - 2];
+  //   this.setData({
+  //     introduce: prepage.data.introduce
+  //   })
+  //   wx.createSelectorQuery().select('#editor-textarea').context(res => {
+  //     res.context.setContents({
+  //       delta: prepage.data.introduce
+  //     })
+  //   }).exec();
+  // },
+  bindNameInput: function(e) {
+    const name = e.detail.value;
+    this.setData({ name });
+  },
+
+  bingDescribeInput: function(e) {
+    const describe = e.detail.value;
+    this.setData({ describe });
+  },
+
+  bindTextAreaInput: function (e) {
+    var introduce = e.detail.delta;
+    // let pages = getCurrentPages();
+    // var prepage = pages[pages.length - 2], describe = e.detail.html;
+    this.setData({ introduce, auto_height: false });
+  },
+
+  
+  submitForm: function() {
+    const data = {
+      name: this.data.name,
+      logo: this.data.logo,
+      describe: this.data.describe,
+      introduce: this.data.introduce,
+      promote_id: this.data.promote.id
+    }
+    console.log(data);
+    Api.fetchPost(Api.editShop, data, (err, res) => {
+      console.log(res);
+      // 保存成功，跳到详情页
+      wx.navigateTo({
+        url: "/pages/brand/pagehome/pagehome"
+      })
     })
-  },
-
-  bindSwitch: function (e) {
-    var state = +e.currentTarget.dataset.index;
-    this.setData({ state });
-  },
-
-  bindInput: function (e) {
-    let i = +e.currentTarget.dataset.index,
-      value = e.detail.value;
-    switch (this.data.state) {
-      case 0:
-        let promote1 = this.data.promote1;
-        switch (i) {
-          case 0:
-            promote1.promote_name = value;
-            break;
-          case 1:
-            promote1.promote_guide = value;
-            break;
-        }
-        this.setData({ promote1 });
-        break;
-      case 1:
-        break;
-    }
-  },
-
-  bindSelect: function (e) {
-    var promote_type = +e.currentTarget.dataset.index;
-    let promote2 = this.data.promote2;
-    promote2['promote_type'] = promote_type;
-    this.setData({ promote2 });
-  },
-
-  editPromote: function () {
-    switch (this.data.state) {
-      case 0:
-        let promote1 = this.data.promote1;
-        if (!promote1.promote_name) {
-          wx.showToast({
-            title: '复制内容不能为空',
-            icon: 'none',
-            duration: 1500
-          });
-          return;
-        }
-        if (!promote1.promote_guide) {
-          wx.showToast({
-            title: '请输入引导用户复制的文案',
-            icon: 'none',
-            duration: 1500
-          });
-          return;
-        }
-        let data = {
-          promote_type: promote1.promote_type,
-          promote_name: promote1.promote_name,
-          promote_guide: promote1.promote_guide
-        };
-        if (promote1.id) {
-          data['id'] = promote1.id;
-        }
-        Api.fetchPost(Api.EditPromote, data, (err, res) => {
-          if (res.status === 200) {
-            wx.navigateBack({
-              delta: 1
-            })
-          } else {
-            wx.showToast({
-              title: res.error,
-              duration: 1500,
-              icon: 'none',
-            });
-          }
-        })
-        break;
-      case 1:
-        break;
-    }
   }
 })
