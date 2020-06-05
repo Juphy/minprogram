6b49353c7a0363d84be77b958aa6a220597e3170
liwei <liwei@huayingjuhe.com>
2020-06-05 23:33:33 +0800

    保存基本逻辑， 还需要修改样式和链接

--- pages/help/help.js
+++ pages/help/help.js
@@ -1,23 +1,43 @@
-var Api = require("../../utils/api.js");
+var api = require("../../utils/api.js");
 // pages/ucenter/help/help.js
 Page({
   data:{
-    helpList: []
+    helpList: {1: [{question: 'aaaa'}, {question: 'aaaa'}, {question: 'aaaa'}], 2: []},
+    options: [],
+    selected: {}
   },
   getHelpList() {
     let that = this;
-    Api.fetchGet(Api.Question, function (res) {
+    api.fetchGet(api.Question, function (err, res) {
       if (res.status === 200) {
         that.setData({
           helpList: res.result
         })
+        console.log(that.data.helpList[1])
       }
     });
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    this.getHelpList()
+    let that = this;
+    api.fetchPost(api.sysOptions, {type: 'question_answer'}, function (err, res) {
+      if (res.status === 200) {
+        that.setData({
+          options: res.result
+        });
+        that.getHelpList()
+      }
+    });
+    // this.getHelpList()
 
+  },
+  select(event) {
+    console.log(event.currentTarget.dataset.id);
+    const selected = {...this.data.selected};
+    selected[event.currentTarget.dataset.id] = !selected[event.currentTarget.dataset.id];
+    this.setData({
+      selected
+    })
   },
   onReady:function(){
     // 页面渲染完成
